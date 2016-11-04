1ef28198f7039ac912d68782aaebcdcaaece1fb7
Joris van Rantwijk <joris@jorisvr.nl>
2016-11-04 09:39:21 +0100

    Reorganize Python directory.

--- python/setup.py
+++ python/setup.py
@@ -7,22 +7,22 @@ import distutils.command.clean
 
 class MyBuild(distutils.command.build.build):
     def run(self):
-        spawn([ 'make' ])
+        spawn([ 'make', '-C', 'ext' ])
         super().run()
 
 class MyClean(distutils.command.clean.clean):
     def run(self):
         super().run()
-        spawn([ 'make', 'clean' ])
+        spawn([ 'make', '-C', 'ext', 'clean' ])
 
 setup(name='equihash_xenoncat',
       version='0.1',
       description='Python wrapper for Xenoncat Equihash solver',
       ext_modules=[
         Extension(name='equihash_xenoncat',
-                  sources=['equihash_xenoncat.c'],
+                  sources=['ext/equihash_xenoncat.c'],
                   extra_compile_args=['-std=gnu11'],
-                  extra_objects=['equihash_lib_avx1.o',
-                                 'equihash_lib_avx2.o']) ],
+                  extra_objects=['ext/equihash_lib_avx1.o',
+                                 'ext/equihash_lib_avx2.o']) ],
       cmdclass={'build': MyBuild, 'clean': MyClean} )
 
